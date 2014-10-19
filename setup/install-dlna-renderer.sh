38bad52cc921390db97eb1265a2cbd2d6995d561
Josh Davis <josh@gfsi.net>
2014-10-19 00:32:18 -0700

    Corrected errors with previous edits

--- setup/install-dlna-renderer.sh
+++ setup/install-dlna-renderer.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 # This will install the DLNA renderer for a Raspberry Pi
+# DON'T RUN THIS SCRIPT AS SUDO - all commands that need to be sudo are taken care of inside of the script
 # Credit goes to Marcin Galkowski (http://rootprompt.apatsch.net/2013/03/07/raspberry-pi-network-audio-player-pulseaudio-dlna-and-bluetooth-a2dp-part-2-dlna/)
 
 # Install dependencies first
@@ -14,12 +17,12 @@ libxml2-dev mpg321
 # Add dependency for new GStreamer
 
 sudo wget -O - http://www.chiark.greenend.org.uk/~christi/debian/christi@coraline.org.gpg.key | sudo apt-key add -
-sudo -sh 'echo "deb http://www.chiark.greenend.org.uk/~christi/debian/ wheezy main" > /etc/apt/sources.list.d/upnprender.list'
+sudo bash -c 'echo "deb http://www.chiark.greenend.org.uk/~christi/debian/ wheezy main" > /etc/apt/sources.list.d/upnprender.list'
 
 # Install updated GStreamer
 
 sudo apt-get update
-sudo apt-get install libupnp-dev libgstreamer1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-alsa
+sudo apt-get -y install libupnp-dev libgstreamer1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-alsa
 
 # Pull down repository
 
@@ -31,11 +34,10 @@ popd
 
 pushd ~/RaspberryPi/gmrender-resurrect
 ./autogen.sh
-./configure --prefix=`pwd` && make install
+./configure && make install
 
 # Binplace
 
-sudo cp -f bin/gmediarender /usr/local/bin
 sudo cp -f scripts/init.d/gmediarenderer /etc/init.d/gmediarenderer
 sudo chmod 755 /etc/init.d/gmediarenderer
 sudo update-rc.d gmediarenderer defaults 93 07
